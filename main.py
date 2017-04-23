e66d7fff7404147190df929d65e357f9b93a81f3
aleoneill <aoneill@csumb.edu>
2017-04-23 17:49:16 +0000

    done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main py file to run flask
 from flask import *
-from schoo_Stat import school_info
+from schoo_Stat import *
 import os # library for system functions
 from werkzeug.utils import secure_filename
 from samples import *
@@ -13,19 +13,19 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 colleges = {
-    "CSU Monterey Bay" : {'data' : {}, "comments" : {}, 'counter' : 0, 'picture' : 'static/campus_pics/Monterey.png'},
-    "San Jose State" : {'data' : {}, "comments" : {}, 'counter' : 0, 'picture' : 'static/campus_pics/CSUMontereyBay.png'},
-    "San Diego State" : {'data' : {}, "comments" : {}, 'counter' : 0, 'picture' : 'static/campus_pics/CSUMontereyBay'},
-    "UC Berkeley" : {'data' : {}, "comments" : {}, 'counter' : 0, 'picture' : 'static/campus_pics/CSUMontereyBay'},
-    "UC Davis" : {'data' : {}, "comments" : {}, 'counter' : 0, 'picture' : 'static/campus_pics/CSUMontereyBay'},
-    "UC Irvine" : {'data' : {}, "comments" : {}, 'counter' : 0, 'picture' : 'static/campus_pics/CSUMontereyBay'}
+    "CSU Monterey Bay" : {'data' : {}, "comments" : {}, 'counter' : 0, 'picture' : 'static/campus_pics/csumb.jpg', 'back_stats' : 'static/campus_pics/Otter.png'},
+    "San Jose State" : {'data' : {}, "comments" : {}, 'counter' : 0, 'picture' : 'static/campus_pics/SJSU.jpg.jpg', 'back_stats' : 'static/campus_pics/Spartan.png'},
+    "San Diego State" : {'data' : {}, "comments" : {}, 'counter' : 0, 'picture' : 'static/campus_pics/SDSU.jpg', 'back_stats' : 'static/campus_pics/Aztec.png'},
+    "UC Berkeley" : {'data' : {}, "comments" : {}, 'counter' : 0, 'picture' : 'static/campus_pics/UCB.jpg', 'back_stats' : 'static/campus_pics/Bear.png'},
+    "UC Davis" : {'data' : {}, "comments" : {}, 'counter' : 0, 'picture' : 'static/campus_pics/UCD.jpg', 'back_stats' : 'static/campus_pics/Aggies.png'},
+    "UC Irvine" : {'data' : {}, "comments" : {}, 'counter' : 0, 'picture' : 'static/campus_pics/UCI.jpg', 'back_stats' : 'static/campus_pics/Anteater.png'}
 }
 
 colleges = call_samples(colleges)
+colleges = statistics(colleges)
 
 class selected():
     selected_college = ""
-    stats = school_info
 x = selected()
 
 #Homepage#######################################
@@ -47,6 +47,9 @@ def college():
         params[i] = colleges[x.selected_college]['comments'][i]
     print params
     params['college'] = x.selected_college
+    params['school_pic'] = colleges[x.selected_college]['picture']
+    params['back_stats'] = colleges[x.selected_college]['back_stats']
+    params['stats'] = colleges[x.selected_college]['data']
     return render_template("college.html", params=params)
 
 @app.route('/comment') # homepage route
